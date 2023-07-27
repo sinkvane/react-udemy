a0c62fbe3b92c64e09172a637c8888fee66840af
Sinkvane <sinkvane@gmail.com>
2023-07-27 17:56:20 +0700

    keys added with React Reconciliation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
   const data = [
-    { name: 'Тимур', salary: 800, increase: true },
-    { name: 'Максим', salary: 200, increase: true },
-    { name: 'Никита', salary: 1000, increase: false },
+    { name: 'Тимур', salary: 800, increase: true, id: 1 },
+    { name: 'Максим', salary: 200, increase: true, id: 2 },
+    { name: 'Никита', salary: 1000, increase: false, id: 3 },
   ];
 
   return (
