0593e7f18e8d05fe8078ffb81ecd24a55d4b450c
Sinkvane <sinkvane@gmail.com>
2023-08-27 22:28:07 +0700

    delete & add new users & immutable state included

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -17,13 +17,25 @@ class EmployeesAddForm extends Component {
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() {
         const {name, salary} = this.state;
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
-                <form className="add-form d-flex">
+                <form
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}
+                >
                     <input type="text"
                            className='form-control new-post-label'
                            placeholder='Как его зовут?'
@@ -38,7 +50,9 @@ class EmployeesAddForm extends Component {
                            value={salary}
                            onChange={this.onValueChange}
                     />
-                    <button type="submit" className="btn btn-outline-light">Добавить</button>
+                    <button type="submit"
+                            className="btn btn-outline-light"
+                    >Добавить</button>
                 </form>
             </div>
         );
